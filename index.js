f021ad86c66c3b215fa346858e7e0568073a8dda
Dan Peddle <dan.peddle@gmail.com>
2013-10-22 23:28:39 +0200

    put in evented calls

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 
 var api = require('./lib/fake-api');
 var requestState = require('./lib/request-state');
+var crc32 = require('./lib/crc32');
 
-var NUM_TESTS = 100;
+var NUM_TESTS = 10;
 var token = 0;
 
+
+// Just to be storing an object, instead of some primitive
+var Token = function(){
+    this.value = Math.floor(Math.random() * Date.now());
+};
+
+Token.prototype.toString = function(){
+    return crc32(this.value);
+};
+
+//  do something with promises returned from the bus
+api.on('completed', function(saving){
+
+    var clsToken = requestState.get('token');
+
+    saving.then(function(data){
+        console.log('ok:', data);
+        assert(clsToken === data.token, 'comparing tokens on completion');
+    },function(err){
+        console.log('prob:', err);
+    })
+});
+
 for (var i = 0; i < NUM_TESTS; i++) {
+
     requestState.run(function(){
-        token++;
+
+        var token = new Token();
         requestState.set('token', token);
-        
-        api.save({
-            time: Date.now()
-        },token).then(function(data){
-            console.log('result:', data);
-        }, function(err){
-            console.error('Prob:', err);
-        });
-        
+        api.emit('save', token);
+
     });
-    
 
 };
 
